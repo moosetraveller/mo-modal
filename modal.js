e6dd5ed2ca37137612aa9e751533290778caf694
Thomas Zuberbuehler <thomas.zuberbuehler@dal.ca>
2024-12-29 11:44:58 -0400

    initial version

---
+++ modal.js
@@ -0,0 +1,182 @@
+// CSS module import does not work unless CSS file is explicit served as text/css
+// import sheet from './modal.css' assert { type: 'css' }; 
+
+import { EventHandler as EH, loadStyleSheet } from "./utils.js";
+
+/**
+ * A simple Modal that could be used to confirm something.
+ */
+class Modal extends HTMLElement {
+
+    static _styleSheet = null;
+
+    /** for communication between modals */
+    static _eventBus = new EventTarget();
+
+    get isOpen() {
+        return this.hasAttribute('opened');
+    }
+
+    constructor() {
+        
+        super();
+        this.attachShadow({mode: 'open'});
+
+        this.shadowRoot.innerHTML = `
+
+            <style id="initialStyle">
+                
+                /* initial style; will be removed when 
+                   CSS style sheet was loaded */
+
+                :host {
+                    display: none;
+                }
+                
+            </style>
+
+            <div id="backdrop"></div>
+            <div id="modal">
+
+                <header>
+                    <slot name="title">Please Confirm</slot>
+                </header>
+
+                <section id="content">
+                    <slot></slot>
+                </section>
+
+                <section id="buttonPanel">
+                    <button id="cancelButton">Cancel</button>
+                    <button id="confirmButton">Confirm</button>
+                </section>
+
+            </div>
+
+        `;
+
+        this._insertStyleSheet('./modal.css');
+
+        this._uuid = crypto.randomUUID();
+
+    }
+
+    /**
+     * Inserts style sheet from external file and makes sure that
+     * it's only loaded/retrieved once even if the component is
+     * used more than once.
+     * 
+     * @param {string} url url pointing to stylesheet
+     */
+    async _insertStyleSheet(url) {
+
+        if (Modal._styleSheet === null) {
+            Modal._styleSheet = loadStyleSheet(url);
+        }
+
+        Modal._styleSheet
+            .then(sheet => {
+                this.shadowRoot.adoptedStyleSheets = [
+                    ...(this.shadowRoot.adoptedStyleSheets || []),
+                    sheet,
+                ];
+                // remove the initial (temporary) style
+                const initStyle = this.shadowRoot.querySelector('#initialStyle');
+                initStyle?.remove();
+            });
+
+    }
+
+    connectedCallback() {
+
+        // using helper function EH.attach to add event listeners, in order to remove
+        // them easily when disconnectedCallback is called
+
+        EH.attach('open', Modal._eventBus, this._enforceExclusiveness.bind(this));
+        
+        this._cancelButton = this.shadowRoot.querySelector('#cancelButton');
+        EH.attach('click.cancel', this._cancelButton, (event) => {
+            this.close();
+            this._emitCancelEvent(event);
+        });
+
+        this._confirmButton = this.shadowRoot.querySelector('#confirmButton');
+        EH.attach('click.confirm', this._confirmButton, (event) => {
+            this.close();
+            this._emitConfirmEvent(event);
+        });
+
+        this._backdrop = this.shadowRoot.querySelector('#backdrop');
+        EH.attach('click', this._backdrop, () => this.close());
+
+        this._render();
+
+    }
+
+    disconnectedCallback() {
+        EH.detachAll(this._confirmButton, this._cancelButton, this._backdrop);
+        EH.detach('open', Modal._eventBus);
+    }
+
+    _render() {
+        this._confirmButton.textContent = this.getAttribute('confirmText') ?? 'Confirm';
+    }
+
+    _enforceExclusiveness(event) {
+        if (this.isOpen && event.detail.uuid !== this._uuid) {
+            this.close();
+        }
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+    }
+
+    close() {
+        this.removeAttribute('opened');
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+
+        if (name === 'opened' && oldValue !== newValue) {
+            if (this.hasAttribute('opened')) {
+                this._emitOpenedEvent();
+            }
+            else {
+                this._emitClosedEvent();
+            }
+        }
+
+        this._render();
+
+    }
+
+    static get observedAttributes() {
+        return ['opened', 'confirmText'];
+    }
+
+    _emitOpenedEvent() {
+        const event = new CustomEvent('open', { detail: { uuid: this._uuid } });
+        this.dispatchEvent(event);
+        Modal._eventBus.dispatchEvent(event);
+    }
+
+    _emitClosedEvent() {
+        // triggered on the custom component itself and not within the shadow DOM
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    _emitCancelEvent(clickEvent) {
+        // we bubble the event up to the custom component itself
+        // if composed is false, the event won't leave the shadow DOM even if bubbles: true
+        // easier to call this.dispatchEvent, but leaving that example for further reference
+        clickEvent.target.dispatchEvent(new CustomEvent('cancel', { bubbles: true, composed: true }));
+    }
+
+    _emitConfirmEvent(clickEvent) {
+        clickEvent.target.dispatchEvent(new CustomEvent('confirm', { bubbles: true, composed: true }));
+    }
+
+}
+
+customElements.define('mo-modal', Modal);
---
+++ utils.js
@@ -0,0 +1,117 @@
+/**
+ * Static functions to add and remove event listeners without the need of having the
+ * same callback function at hand. 
+ * 
+ * When attaching an event listener, you may extend the event type with a name. Simply
+ * separate the type and name with a dot (`.`). For example: `click.cancel`. 
+ * 
+ * The event type is required to remove a listener. If a name was used, then the name 
+ * must be provided too (e.g., `click`, `click.cancel` or `mouseout`).
+ */
+export class EventHandler {
+
+    static _abortControllers = new WeakMap();
+
+    /**
+     * Attach an event listener for the given event type to the given event target.
+     * 
+     * @param {string} eventType the event type, optionally followed by a name separated by a dot (.)
+     * @param {EventTarget} eventTarget the event target where the event listener will be attached
+     * @param {EventListenerOrEventListenerObject} callback the callback function
+     */
+    static attach(eventType, eventTarget, callback) {
+
+        if (!(eventTarget instanceof EventTarget)) {
+            return;
+        }
+
+        const controller = new AbortController();
+
+        const controllers = EventHandler._abortControllers.get(eventTarget) ?? new Map();
+        controllers.set(eventType, controller);
+
+        eventTarget.addEventListener(eventType.split('.')[0], callback, { signal: controller.signal });
+
+        EventHandler._abortControllers.set(eventTarget, controllers);
+
+    }
+
+    /**
+     * Detach the event listener for the given event type from the given event targets.
+     * 
+     * @param {string} eventType the event type, optionally followed by a name separated by a dot (.)
+     * @param {...EventTarget} eventTargets the event targets from which the event listener should be removed
+     */
+    static detach(eventType, ...eventTargets) {
+        
+        for (const eventTarget of eventTargets) {
+
+            if (!(eventTarget instanceof EventTarget)) {
+                continue;
+            }
+
+            const controllers = this._abortControllers.get(eventTarget) ?? false;
+
+            if (controllers === false) {
+                continue;
+            }
+
+            const controller = controllers.get(eventType);
+            
+            controller.abort();
+
+            if (controllers.delete(eventType) && controller.size === 0) {
+                this._abortControllers.delete(eventTarget);
+            }
+
+        }
+
+    }
+
+    /**
+     * Detach all event listeners from the given event targets.
+     * 
+     * @param {...EventTarget} eventTargets the event targets from which the event listener should be removed
+     */
+    static detachAll(...eventTargets) {
+        
+        for (const eventTarget of eventTargets) {
+
+            if (!(eventTarget instanceof EventTarget)) {
+                continue;
+            }
+
+            const controllers = this._abortControllers.get(eventTarget) ?? false;
+
+            if (controllers === false) {
+                continue;
+            }
+
+            for (const controller of controllers.values()) {
+                controller.abort();
+            }
+
+            controllers.clear();
+            this._abortControllers.delete(eventTarget);
+
+        }
+
+    }
+
+}
+
+/**
+ * Loads a stylesheet from the given URL.
+ * 
+ * @param {string} url url pointing to stylesheet
+ * @returns {Promise<CSSStyleSheet>} a promise
+ */
+export async function loadStyleSheet(url) {
+    return new Promise(async (resolve, _) => {
+        const sheet = new CSSStyleSheet();
+        const response = await fetch(url);
+        const styles = await response.text();
+        sheet.replaceSync(styles)
+        resolve(sheet);
+    });
+}
